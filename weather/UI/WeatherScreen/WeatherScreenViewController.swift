eb77965a610c56a6065e88d6dfbb5da333f4e2e6
Kito <stakanchik86@gmail.com>
2022-07-30 18:18:06 +0300

    add assets

--- weather/UI/WeatherScreen/WeatherScreenViewController.swift
+++ weather/UI/WeatherScreen/WeatherScreenViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 class WeatherScreenViewController: UIViewController {
     
 //MARK: IBOutlet
-    
     @IBOutlet private var navigationView: UIView!
     /// DayInfoView
-    ///
-    /// 
+    @IBOutlet private var dayInfoViewConteiner: UIView!
+    @IBOutlet private var dayInfoTitleLabel: UILabel!
+    @IBOutlet private var dayInfoImageVIew: UIImageView!
     /// StackViews
     @IBOutlet private var hoursInfoStackView: UIStackView!
-    @IBOutlet var weekInfoStackView: UIStackView!
+    @IBOutlet private var weekInfoStackView: UIStackView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupUI()
         // Do any additional setup after loading the view.
     }
-
+    
+    private func setupUI() {
+        
+        navigationView.backgroundColor = UIColor(named: "mainColor")
+        dayInfoViewConteiner.backgroundColor = UIColor(named: "mainColor")
+        dayInfoTitleLabel.text = "Text 1"
+        dayInfoImageVIew.image = UIImage(named: "ic_white_day_bright")
+        dayInfoImageVIew.tintColor = .white
+        
+    }
 }
